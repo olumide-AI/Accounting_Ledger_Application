34ed1fee62ead5d8878268776586f8d553e1f1ec
olumide-AI <olumidekolawole26@gmail.com>
2025-04-26 05:04:30 -0700

    I have added a constructor transaction for modularity in my code i have create date,time,description,vendor,amount fields for the constructor. I have a parameterless and a full field parameter constructors, I have made all my fields private and create getter and setter methods for encapsulation.

---
+++ src/main/java/accountingledgerapplication/Transaction.java
@@ -0,0 +1,60 @@
+package accountingledgerapplication;
+
+public class Transaction {
+    //Should Date and time be type datetime
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    //Create constructor
+    public Transaction(String date, String time, String description, String vendor, double amount){
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+    //Getters and Setters
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
