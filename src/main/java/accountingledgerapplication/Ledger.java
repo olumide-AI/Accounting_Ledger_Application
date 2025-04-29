5fe118eab701567ea23db98beeb715fc00b054dd
olumide-AI <olumidekolawole26@gmail.com>
2025-04-29 09:55:06 -0700

    copied and pasted ledger code into it's own ledger class

--- src/main/java/accountingledgerapplication/Ledger.java
+++ src/main/java/accountingledgerapplication/Ledger.java
@@ -1,4 +1,37 @@
 package accountingledgerapplication;
 
+import java.util.List;
+
 public class Ledger {
+
+    //Ledger displays all entries, so we read from the csv file using buffered reader method
+    public static void displayAllEntries(){
+        List<Transaction> transactionList = FileHandler.readAllTransactions();
+        System.out.println("Here is all the transaction for your account: ");
+        for (Transaction transaction : transactionList) {
+            System.out.println(transaction.displayTransactionFormat());
+        }
+    }
+
+    //Method that display only deposit transaction to console
+    public static void displayDepositEntries(){
+        List<Transaction> transactionList = FileHandler.readAllTransactions();
+        System.out.println("Here is all the Deposit transaction entries for your account: ");
+        for (Transaction transaction : transactionList) {
+            if (transaction.getAmount() > 0) {
+                System.out.println(transaction.displayTransactionFormat());
+            }
+        }
+    }
+
+    //Display only debits entries
+    public static void displayDebitPaymentEntries(){
+        List<Transaction> transactionList = FileHandler.readAllTransactions();
+        System.out.println("Here is all the Debit transaction entries for your account: ");
+        for (Transaction transaction : transactionList) {
+            if (transaction.getAmount() < 0) {
+                System.out.println(transaction.displayTransactionFormat());
+            }
+        }
+    }
 }
